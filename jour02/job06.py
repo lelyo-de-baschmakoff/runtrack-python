f4c039b1320ba50db1d35906d92151328fd381af
lelyo-de-baschmakoff <lelyo.de-baschmakoff@laplateforme.io>
2023-11-21 10:46:35 +0100

    job06

---
+++ jour02/job06.py
@@ -0,0 +1,16 @@
+def est_nombre_premier(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def afficher_nombres_premiers(maximum):
+    print("Nombres premiers jusqu'à", maximum, ":")
+    for nombre in range(2, maximum + 1):
+        if est_nombre_premier(nombre):
+            print(nombre, end=" ")
+
+# Afficher les nombres premiers jusqu'à 1000
+afficher_nombres_premiers(1000)
